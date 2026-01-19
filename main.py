54e2949f5ae5c3ed8d07b00c432c5d65c0853a95
Shahzod <sen@example.com>
2026-01-19 10:23:45 +0500

    Lugat dasturi qo‘shildi

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+while True:
+   a=input('sozni kiriting')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   lugat ={"быть": 'bolmoq',
+        'мочь':'qodir bolmoq'}
+   if a in lugat.values():
+       for k, v in lugat.items():
+           if v == a:
+               print(k)
+   else:
+       print('bunday soz yoq')
